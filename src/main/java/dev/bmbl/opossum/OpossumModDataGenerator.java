91f5d34658facb442a24325a43d7b2d820050836
Walker Sakatch <walkersakatch@gmail.com>
2025-02-14 19:27:58 -0700

    initial commit

---
+++ src/main/java/dev/bmbl/opossum/OpossumModDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.bmbl.opossum;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OpossumModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
